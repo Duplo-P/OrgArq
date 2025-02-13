58653a05a8d5dc0aa4a665bf290bad7f87a0726f
Duplo-P <petroithnmanuel@gmail.com>
2025-02-13 20:34:00 +0100

    check Mate

--- modulo.py
+++ modulo.py
@@ -4,10 +4,10 @@ from tkinter import messagebox
 import os
 
 
-extensoes_imagem = ('.jpg', '.jpeg', '.JPG', '.JPEG', '.png', '.PNG', '.gif', '.GIF', '.bmp', '.BMP', '.tiff', '.TIFF', '.svg', '.SVG', '.webp', '.WEBP')
-extensao_video = ('.mp4', '.avi', '.mkv', '.mov', '.wmv', '.flv', '.webm', '.mpeg', '.mpg', '.m4v')
-extensao_doc = ('.pdf', '.doc', '.docx', '.txt', '.xls', '.xlsx', '.ppt', '.pptx', '.odt', '.ods', '.odp')
-extensao_zip = ('.zip', '.rar', '.7z', '.tar', '.gz', '.bz2', '.xz')
+extensoes_imagem = ('.jpg', '.jpeg', '.JPG', '.JPEG', '.png', '.PNG', '.gif', '.GIF', '.bmp', '.BMP', '.tiff', '.TIFF', '.svg', '.SVG', '.webp', '.WEBP','.heic', '.HEIC', '.ico', '.ICO', '.jfif', '.JFIF', '.pjpeg', '.PJPEG', '.pjp', '.PJP', '.avif', '.AVIF')
+extensao_video = ('.mp4','.MOV','.avi', '.mkv', '.mov', '.wmv', '.flv', '.webm', '.mpeg', '.mpg', '.m4v','.MP4','.MOV','.AVI', '.MKV', '.MOV', '.WMV', '.FLV', '.WEBM', '.MPEG', '.MPG', '.M4V', '.hevc', '.HEVC', '.3gp', '.3GP', '.3g2', '.3G2', '.asf', '.ASF', '.avchd', '.AVCHD', '.flv', '.FLV', '.m2ts', '.M2TS', '.mts', '.MTS', '.mxf', '.MXF', '.ogg', '.OGG', '.rm', '.RM', '.rmvb', '.RMVB', '.ts', '.TS', '.vob', '.VOB', '.webm', '.WEBM', '.wmv', '.WMV')
+extensao_doc = ('.pdf','.PDF' ,'.doc', '.docx', '.txt', '.xls', '.xlsx', '.ppt', '.pptx', '.odt', '.ods', '.odp','.odg', '.odf', '.odc', '.odb', '.odm', '.ott', '.ots', '.otp', '.otg', '.otf', '.otc', '.otb', '.oth', '.docm', '.dot', '.dotx', '.dotm', '.docb', '.xlsb', '.xlsm', '.xltx', '.xltm', '.xlsx', '.xlsm', '.xlt', '.xlm', '.xlw', '.pptm', '.potx', '.potm', '.ppam', '.ppsx', '.ppsm', '.sldx', '.sldm', '.pot', '.pps', '.ppa', '.ppam', '.ppsx', '.ppsm', '.sldx', '.sldm', '.pot', '.pps', '.ppa', '.ppam', '.ppsx', '.ppsm', '.sldx', '.sldm', '.pot', '.pps', '.ppa', '.ppam', '.ppsx', '.ppsm', '.sldx', '.sldm', '.pot', '.pps', '.ppa', '.ppam', '.ppsx', '.ppsm', '.sldx', '.sldm', '.pot', '.pps', '.ppa', '.ppam', '.ppsx', '.ppsm', '.sldx', '.sldm', '.pot', '.pps', '.ppa', '.ppam', '.ppsx', '.ppsm', '.sldx', '.sldm', '.pot', '.pps', '.ppa', '.ppam', '.ppsx', '.ppsm', '.sldx', '.sldm', '.pot', '.pps', '.ppa', '.ppam', '.ppsx', '.ppsm', '.sldx', '.sldm', '.pot', '.pps', '.ppa', '.ppam', '.ppsx', '.ppsm', '.sldx', '.sldm', '.pot', '.pps', '.ppa', '.ppam', '.ppsx', '.ppsm', '.sldx', '.sldm', '.pot', '.pps', '.ppa', '.ppam', '.ppsx', '.ppsm', '.sldx', '.sldm', '.pot', '.pps', '.ppa', '.ppam')
+extensao_zip = ('.zip', '.rar', '.7z', '.tar', '.gz', '.bz2', '.xz, .tgz', '.tar.gz', '.tar.bz2', '.tar.xz','.iso')
 extensao_exe = ('.exe', '.bat', '.sh', '.bin', '.cmd', '.com', '.msi', '.jar', '.py', '.pl')
 extensao_audio = ('.mp3', '.wav', '.flac', '.aac', '.ogg', '.wma', '.m4a', '.aiff', '.alac')
 
